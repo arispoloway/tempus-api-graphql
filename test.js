b08fa233797af8b14327081c4002bcc01bf05feb
Aris Poloway <agpoloway@gmail.com>
2020-07-26 20:15:44 -0400

    Remove test file

--- test.js
+++
@@ -1,7 +0,0 @@
-import { graphql } from 'graphql';
-import schema from './lib/schema.js';
-
-(async () => {
-    let r = await graphql(schema, ' { record(id: 3878771){ duration demo { filename map { name authors { name } } }}}');
-    console.log(JSON.stringify(r, null, "  "));
-})();
