279fbb2611b6a1d70148ac0e6dee7b6fa2b8d3f3
Aris Poloway <agpoloway@gmail.com>
2020-08-30 17:37:52 -0400

    Add SDL dump script and save schema.graphql (#15)

---
+++ scripts/dump_sdl.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const { printSchema } = require("graphql");
+const schema = require("../dist/schema").default;
+
+const FILE_NAME = "schema.graphql";
+
+fs.writeFile(FILE_NAME, printSchema(schema), () => {
+  /* eslint-disable no-console */
+  console.log(`SDL saved to ${FILE_NAME}`);
+  /* eslint-enable no-console */
+});
